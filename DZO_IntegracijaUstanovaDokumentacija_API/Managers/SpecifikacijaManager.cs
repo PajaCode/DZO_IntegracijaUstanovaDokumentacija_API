3e505aa287541dc8e895fe571025f8cdcf210af3
Marija Jesic <marija.jesic@BGL-MARIJAJE>
2024-06-27 16:24:54 +0200

    ParsirajIinsertujAsync - 2

--- DZO_IntegracijaUstanovaDokumentacija_API/Managers/SpecifikacijaManager.cs
+++ DZO_IntegracijaUstanovaDokumentacija_API/Managers/SpecifikacijaManager.cs
@@ -82,7 +82,7 @@ namespace DZO_IntegracijaUstanovaDokumentacija_API.Managers
                             try
                             {
                                 _ = InsertPodatakaIzJsona(jsonObject, rezultatItem.Id);
-                                logovi.AzurirajStatusVizimJson(rezultatItem.Id, 3);
+                                logovi.AzurirajStatusVizimJson(rezultatItem.Id, 2);
                             }
                             catch (Exception ex)
                             {
